c461b148c3eb20d2e27d17000e32126d2c0df14f
Smolldo <maximqwerbich@gmail.com>
2021-03-14 19:50:56 +0200

    beta_1
    need some correction

--- script/script.js
+++ script/script.js
@@ -32,7 +32,79 @@ function mathematica(){
 
     V = p * r * 2 * h;
 
-    document.querySelector('.res').innerHTML = Math.round(V);
+    if(V >= 0){
+        document.querySelector('.res').innerHTML = Math.round(V);
+    }
+    else{
+        document.querySelector('.res').innerHTML = "Wrong action";
+    }
 
 
+}
+
+document.querySelector('.btn_three').onclick = myFunc;
+
+function myFunc(){
+    let r = document.querySelector('.third_input').value;
+    let h = document.querySelector('.fourth_input').value;
+
+    let p = Math.PI, S;
+
+    S = 2 * p * Number(r) * (Number(r + h));
+
+    if(S >= 0){
+        document.querySelector('.res1').innerHTML = Math.round(S);
+    }
+    else{
+        document.querySelector('.res1').innerHTML = "Wrong action";
+    }
+}
+
+document.querySelector('.btn_four').onclick = allMost;
+
+function allMost(){
+    let N = document.querySelector('.fifth_input').value;
+
+    let r = Number(N) - 6, l, S, p = Math.PI;
+
+    l = 2 * p * r;
+
+    S = p * Math.pow(r , 2);
+
+    if(l >= 0){
+    document.querySelector('.resultat').innerHTML = Math.round(l);
+    }
+    else{
+    document.querySelector('.resultat').innerHTML = "Wrong Action";
+    }
+
+    if (S >= 0) {
+        document.querySelector('.resultat1').innerHTML = Math.round(S);
+    } else {
+        document.querySelector('.resultat1').innerHTML = "Wrong Action";
+    }
+
+}
+
+document.querySelector('.btn_five').onclick = tasks;
+
+function tasks(){
+    let N = document.querySelector('.sixth_input').value;
+
+    let a = 4 * Number(N) -13, b = 2 * Number(N) - 6;
+    let sum, min, mult, divide, pover, module;
+
+    sum = a + b;
+    min = a - b;
+    mult = a * b;
+    divide = a / b;
+    pover = Math.pow(a, b);
+    module = a % b;
+
+    document.querySelector('.plus').innerHTML = Math.round(sum);
+document.querySelector('.minus').innerHTML = Math.round(min);
+document.querySelector('.multiply').innerHTML = Math.round(mult);
+document.querySelector('.divide').innerHTML = Math.round(divide);
+document.querySelector('.pow').innerHTML = Math.round(pover);
+document.querySelector('.module').innerHTML = Math.round(module);
 }
