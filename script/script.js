9cc81ded9d79aab54ed3de5b217e2a42049a02e5
Smolldo <maximqwerbich@gmail.com>
2021-03-14 17:19:00 +0200

    make 2 tasks
    olla

--- script/script.js
+++ script/script.js
@@ -6,13 +6,33 @@ document.querySelector('.btn').onclick = myClick;
 function myClick(){
     let a = document.querySelector('.first_input').value;
 
-    let x, y, z;
-    x = a;
+    let x = a, y, z;
     y = 2 * a + 1;
     z = a + 2;
 
     x += y - a++ * z;
+    z = a++ + y * 5;
+    y = y - a++ *z;
     console.log(x, y, z);
 
-    document.querySelector('span').innerHTML = x;
+
+    document.querySelector('.ox').innerHTML = a;
+    document.querySelector('.oz').innerHTML = z;
+    document.querySelector('.oy').innerHTML = y;
+}
+
+document.querySelector('.btn_two').onclick = mathematica;
+
+function mathematica(){
+    let N = document.querySelector('.second_input').value;
+
+    let r = 2*N, h, V, p = Math.PI;
+    h =  Number(N) + 4;
+    console.log(N, r, h, p);
+
+    V = p * r * 2 * h;
+
+    document.querySelector('.res').innerHTML = Math.round(V);
+
+
 }
